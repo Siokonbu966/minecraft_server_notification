e67401709b519f1d3bcf3d1b45a4e0bed9fe5904
Siokonbu <167207736+N-Siokonbu@users.noreply.github.com>
2025-05-25 21:22:14 +0900

    起動時と停止時にdiscordに通知を投げるbot

---
+++ stop.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER_BOOT_CHANNEL_ID = int(os.getenv('SERVER_CHANNEL_ID'))
+
+class NotificationClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} (通知クライアント) としてログインしました')
+        channel = self.get_channel(SERVER_BOOT_CHANNEL_ID)
+        await channel.send("📢 鯖閉じたよ～ ⛔")
+        await self.close()  # メッセージ送信後にクライアントを閉じる
+
+async def main():
+    client = NotificationClient(intents=discord.Intents.default())
+    await client.start(TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
