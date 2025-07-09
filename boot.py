99fc65d86aba0f82a1c127fe0755c6efbd18d775
Siokonbu <167207736+N-Siokonbu@users.noreply.github.com>
2025-07-09 19:34:03 +0900

    update: ループを修正

--- boot.py
+++ boot.py
@@ -5,20 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)  # 環境変数を上書きするために override=True を指定
 TOKEN = os.getenv('DISCORD_TOKEN')
-SERVER_BOOT_CHANNEL_ID = int(os.getenv('SERVER_CHANNEL_ID'))
+CHANNEL_ID_SHAREHOUSE = int(os.getenv('SERVER_CHANNEL_ID_SHAREHOUSE'))
+CHANNEL_ID_SIOKONBU = int(os.getenv('SERVER_CHANNEL_ID_SIOKONBU'))
 
-print(f"DISCORD_TOKEN: {TOKEN}")
-print(f"SERVER_CHANNEL_ID: {SERVER_BOOT_CHANNEL_ID}")
 class NotificationClient(discord.Client):
     async def on_ready(self):
         print(f'{self.user} (通知クライアント) としてログインしました')
-        channel = self.get_channel(SERVER_BOOT_CHANNEL_ID)
-        await channel.send("📢 鯖建てたよ～ ▶")
+        channel_share = self.get_channel(CHANNEL_ID_SHAREHOUSE)
+        await channel_share.send("📢 鯖建てたよ～ ▶")
+        channel_sio = self.get_channel(CHANNEL_ID_SIOKONBU)
+        await channel_sio.send("📢 鯖建てたよ～ ▶")
         await self.close()  # メッセージ送信後にクライアントを閉じる
 
-async def main():
-    client = NotificationClient(intents=discord.Intents.default())
-    await client.start(TOKEN)
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    client = NotificationClient(intents=discord.Intents.default())
+    client.run(TOKEN)
